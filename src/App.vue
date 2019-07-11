fe548a198476f9a27e177b0dec35264ca83c49ae
michel.luis <michel.luis@pullup.tech>
2019-07-11 19:30:44 -0300

    header pronto

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,6 @@ export default {
 </script>
 
 <style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i&display=swap');
   @import "./assets/sass/style.scss";
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="header">
-    <div class="logo">
-      <img :src="require('../assets/breaking-bad-logo.png')" alt="logo">
-    </div>
-    <div class="menu">
-
-    </div>
-    <div class="search">
-
+  <div class="component-header">
+    <div class="container">
+      <div class="header">
+        <div class="logo">
+          <img :src="require('../assets/img/breaking-bad-logo.png')" alt="logo">
+        </div>
+        <div class="menu">
+          <ul class="nav">
+            <li class="item-nav active">
+              <a href="">Personagens</a>
+            </li>
+            <li class="item-nav">
+              <a href="">Episodios</a>
+            </li>
+            <li class="item-nav">
+              <a href="">Sugestão</a>
+            </li>
+          </ul>
+        </div>
+        <div class="search">
+          <form action="" method="get">
+            <input type="text" name="characters" id="" placeholder="Pesquise os personagens">
+            <button class="submit" type="submit"></button>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 </template>
